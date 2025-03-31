a3108946b6c0a74bc164ea8e1ff9845232dc04ba
dmskhs0912 <dmskhs0912@gmail.com>
2025-03-31 13:59:25 +0900

    update scrap.html 전체 스크랩 데이터 출력력

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import scrap
+import scrap, search
 from flask import Flask, render_template, request
 
 
@@ -9,18 +9,22 @@ def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['GET', 'POST'])
-def search():
+def search_page():
     if request.method == 'GET':
         return render_template('search.html')
     
     elif request.method == 'POST':
-        search_text = request.form.get('search_text')
-        return render_template('search.html', search_text=search_text)
+        stock_title = request.form.get('search_text')
+        # search.search_by_title(stock_title)이 구현되어있다고 가정. stock_title을 종목명으로 하는 주식 API 이용해서 검색.
+        # 반환 데이터 형식은 딕셔너리. {'title': '...', 'price': '...'}
+        #search_data = search.search_by_title(stock_title)
+        return render_template('search.html', stock_title=stock_title)
 
 @app.route('/scrap', methods=['GET', 'POST'])
 def scrap_page():
     if request.method == 'GET':
-        return render_template('scrap.html')    
+        data = scrap.fetch_headlines()
+        return render_template('scrap.html', data=data)    
     elif request.method == 'POST':
         scrap_search = request.form.get('scrap_search')
         print(scrap_search)
